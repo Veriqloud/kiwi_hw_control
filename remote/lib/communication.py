560fba795a32ba629be8dcdbb8c496c00ca0bcde
Georg Harder <harder.georg@gmail.com>
2026-02-09 07:51:17 +0100

    reworking python communication..

--- remote/lib/communication.py
+++ remote/lib/communication.py
@@ -1,14 +1,58 @@
+import sys
+sys.path.insert(0, '/home/vq-user/hw_control/lib')
+
 import socket, json, os, time
-from lib.visuals import mylogger
+from visuals import mylogger
 from enum import Enum
 
 
-# receive an exact number of bytes
-def rcv_exact(conn, l):
-    m = bytes(0)
-    while len(m)<l:
-        m += conn.recv(l - len(m))
-    return m
+# define commands here to avoid spelling issues
+class HwsCommand(Enum):
+    SAVE= "save"
+    LOAD= "load"
+    CLEAN= "clean"
+
+# identify message content based on this
+class MessageType(Enum):
+    COMMAND= 1
+    STR= 2
+    INT_32= 3
+    INT_64= 4
+    DOUBLE= 5
+
+
+# add some function to a socket instance
+# original existing functions: send, sendall, recv
+class ExtendSocket():
+    def __init__(self, sock):
+        self._base = sock
+    
+    def __getattr__(self, name):
+        return getattr(self._base, name)
+
+    # receive an exact number of bytes
+    def rcv_exact(self, l):
+        m = bytes(0)
+        while len(m)<l:
+            m += self.recv(l - len(m))
+        return m
+
+    def psnd(self, data):
+        """
+        package and send
+        data: string, int, float, bytes
+        """
+        if type(data) == HwsCommand:  
+            m = bytes(MessageType.COMMAND)
+        else:
+            exit("did not recognize data type")
+        print(m)
+        self.send(m)
+    
+    def prcv(self):
+        """
+        receive package and return data
+        """
 
 
 class TcpServer():
@@ -55,11 +99,10 @@ class TcpServer():
         self.sock.listen()
         self.logger.info(f"listening on {host}:{port}")
 
-    # handle client in background
     def accept(self):
-        conn, addr = self.sock.accept()  # Accept incoming connection
+        conn, addr = self.sock.accept()
         self.logger.info(f"connected by {addr}")
-        return (conn, addr)
+        return ExtendSocket(conn)
 
 
 
@@ -89,8 +132,8 @@ class TcpClient():
                 exit()
             with open(ports_for_localhost_file, 'r') as f:
                 ports_for_localhost = json.load(f)
-                host = 'localhost'
-                port = ports_for_localhost[program_name]
+                self.host = 'localhost'
+                self.port = ports_for_localhost[program_name]
         else:
             network_file = os.path.join(self.osenv['QLINE_CONFIG_DIR'], player+'/network.json')
             if not os.path.exists(network_file):
@@ -99,48 +142,29 @@ class TcpClient():
             with open(network_file, 'r') as f:
                 network = json.load(f)
             myname = network['myname']
-            port = network['port'][program_name]
-            host = network['ip'][myname]
+            self.port = network['port'][program_name]
+            self.host = network['ip'][myname]
 
         self.socket = socket.socket()
+
+    def connect(self):
         # block and retry if socket cannot be connected to 
         while True:
             try:
-                self.socket.connect((host, port))
-                self.logger.info(f"connected to {host}:{port}")
+                self.socket.connect((self.host, self.port))
+                self.logger.info(f"connected to {self.host}:{self.port}")
                 break
             except Exception as e:
-                self.logger.warning(f"Cannot connect to socket on {host}:{port} due to error: {e}. Retrying in 5 sec.")
+                self.logger.warning(f"Cannot connect to socket on {self.host}:{self.port} due to error: {e}. Retrying in 5 sec.")
                 time.sleep(5)
                 continue
-
-    def send(self, message):
-        """
-        message: 
-        """
-        if type(message) == HwsCommand:  
-            print("ok")
-        else:
-            print(type(message))
-
-        #self.socket.sendall(data)
+        return ExtendSocket(self.socket)
 
 
 
 
 
-# define commands here to avoid spelling issues
-class HwsCommand(Enum):
-    SAVE= "save"
-    LOAD= "load"
-    CLEAN= "clean"
 
-class MessageType(Enum):
-    COMMAND= 1
-    STR= 2
-    INT_32= 3
-    INT_64= 4
-    DOUBLE= 5
 
 
 
@@ -152,15 +176,14 @@ def main():
     import threading
 
     def alice():
-        server = TcpServer("hw", "alice")
-        conn, addr = server.accept()
+        conn = TcpServer("hw", "alice").accept()
         m = conn.rcv_exact(4)
         print(m)
 
     def client():
-        conn = TcpClient("hw", "alice")
+        conn = TcpClient("hw", "alice").connect()
         c = HwsCommand.SAVE
-        conn.send(c)
+        conn.psnd(c)
 
     t1 = threading.Thread(target=alice)
     t2 = threading.Thread(target=client)
