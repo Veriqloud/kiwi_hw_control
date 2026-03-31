924a623ba9f25ad37e9851bd7373a0a2de828969
Georg Harder <harder@veriqloud.fr>
2026-03-31 09:49:51 +0200

    [hw] rng print message

--- remote/rng_fpga/rng2fpga.c
+++ remote/rng_fpga/rng2fpga.c
@@ -77,7 +77,7 @@ void aes_ctr_rng(uint8_t *buf, int len) {
 int main(){
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
-    printf("%d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    printf("%d-%02d-%02d %02d:%02d:%02d starting program\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
     fflush(stdout);
 
 	//printf("Open SwiftPro RNG device! \n");
